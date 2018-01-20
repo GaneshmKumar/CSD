6c30316dc9e34c30e782412110ed4196344d1d15
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2018-01-20 09:39:41 +0530

    setup argparse modified configured json files

---
+++ src/argparser.py
@@ -0,0 +1,40 @@
+""" External imports """
+import argparse
+
+"""
+Initilizes argument parser
+"""
+def init_parser():
+    parser = argparse.ArgumentParser(description='Codechef Solutions Downloader')
+    
+    #problem_code
+    parser.add_argument(
+        '-pc', 
+        type=str, 
+        help='The unique code for the problem', 
+        required=True) 
+
+    #page    
+    parser.add_argument(
+        '-p', 
+        type=int, 
+        default=-1, 
+        help='Number of pages to download')
+
+    #language    
+    parser.add_argument(
+        '-l', 
+        nargs='+', 
+        type=str, 
+        default=['All'], 
+        help='Solutions wiht the specified languages')
+
+     #status_code
+    parser.add_argument(
+        '-sc', 
+        nargs='+', 
+        type=str, 
+        default=['AC'], 
+        help='Solutions with specific status')
+    
+    return parser
--- src/codechef.py
+++ src/codechef.py
@@ -1,9 +1,14 @@
-""" Built in imports """
-from string import Template
-
 """ Exteral imports """
 from bs4 import BeautifulSoup
 import requests
+import json
+
+""" Built in imports """
+from string import Template
+
+""" Custom imports """
+import argparser
+
 
 """ Dictionary to store url type keys """
 URL_TYPES = {
@@ -19,6 +24,12 @@ URLS = {
     "solutions": Template("$status?page=$page&language=$language&status=$status_code&handle=&Submit=GO")
 }
 
+CONFIG_FILES = {
+    "EXTENSIONS": "extensions.json",
+    "LANGUAGE_CODES": "language_codes.json",
+    "STATUS_CODES": "status_codes.json"
+}
+
 """
 returns a url of requested type
 """
@@ -30,3 +41,32 @@ def get_url(url_type, **params):
     elif url_type == URL_TYPES['SOLUTIONS']:
         return URLS[URL_TYPES['SOLUTIONS']].substitute(status=get_url('status', problem_code=params['problem_code']), page=params['page'], language=params['language'], status_code=params['status_code'])      
 
+"""
+return the soup data of the given url
+"""
+def get_url_data(url):
+    r = requests.get(url)
+    return BeautifulSoup(r.text)
+
+"""
+returns json file content
+"""
+def load_json_file(json_file):
+    config_folder = 'config/'
+    return json.load(open(config_folder + json_file))
+
+if __name__ == "__main__":
+    parser = argparser.init_parser()
+    args = parser.parse_args()
+    
+    pages = args.p 
+    languages = map(lambda x: x.upper(), args.l)
+    status_codes = map(lambda x: x.upper(), args.sc)
+    problem_code = args.pc.upper()
+
+    extensions = load_json_file(CONFIG_FILES["EXTENSIONS"]) # .java, .c, etc ...
+    language_codes = load_json_file(CONFIG_FILES["LANGUAGE_CODES"]) # JAVA, C, PERL, etc ...
+    status_codes = load_json_file(CONFIG_FILES["STATUS_CODES"]) # AC, WA, TLE, etc ...
+
+    
+    
