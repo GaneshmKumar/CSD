7dcd672513692eb4598e36f0a9493f6d6a0ea7f4
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2018-01-20 20:05:11 +0530

    Added code to get solution links and user names

--- src/codechef.py
+++ src/codechef.py
@@ -5,9 +5,12 @@ import requests
 """ Built in imports """
 from string import Template
 import sys
+import re
 
 """ Custom imports """
 from exception import ProblemNotFoundException
+from exception import InvalidLanguageException
+from exception import InvalidStatusException
 import argparser
 import utils
 
@@ -55,18 +58,68 @@ def get_url_data(url):
             sys.exit()
     return BeautifulSoup(r.text, "html.parser")
 
+def is_solutions_available(data):
+    return str(data).find('No Recent Activity') > -1
+
+def get_total_solution_pages(data):
+    page_info = data.find("div", {"class": "pageinfo"})
+    if is_solutions_available(data):
+        return 0
+    elif page_info == None:
+        return 1
+    else:
+        return int(page_info.text.split('of')[1])
+
+def get_user_names(solutions_table):
+    user_link_tags = [a for a in solutions_table.findAll("a", {"href": re.compile("/users/*")})]
+    user_links = [link_tag['href'] for link_tag in user_link_tags]
+    user_names = [link.split('/')[2] for link in user_links]
+    return user_names
+
+def get_solution_links(solutions_table):
+    solution_link_tags = [a for a in solutions_table.findAll("a", {"href": re.compile("/viewsolution/*")})]
+    solution_links = [link_tag['href'] for link_tag in solution_link_tags]
+    return solution_links
+
+def get_solution_details(data):
+    solutions_table = data.find("table", {"class": "dataTable"})
+    user_names = get_user_names(solutions_table)
+    solution_links = get_solution_links(solutions_table)
+    return {"user_names": user_names, "solution_links": solution_links}
+    
+
+def get_solutions(pages, languages, status, problem_code, language_codes, status_codes, extensions):
+    try:
+        for lang_key in languages:
+            if lang_key not in language_codes:
+                raise InvalidLanguageException("Error: There is no language named " + lang_key)
+            
+            for status_key in status:
+                if status_key not in status_codes:
+                    raise InvalidStatusException("Error: There is no status named " + status_key)
+                url = get_url(URL_TYPES['SOLUTIONS'], problem_code=problem_code, language=language_codes[lang_key], status_code=status_codes[status_key], page=0)
+                data = get_url_data(url)
+                pages = get_total_solution_pages(data)
+                
+                for page in range(0, pages):
+                    url = get_url(URL_TYPES['SOLUTIONS'], problem_code=problem_code, language=language_codes[lang_key], status_code=status_codes[status_key], page=page)
+                    data = get_url_data(url)
+                    solution_details = get_solution_details(data)
+    except (ProblemNotFoundException, InvalidLanguageException, InvalidStatusException) as e:
+        print ''.join(e.args)
+        sys.exit()                
+    
 if __name__ == "__main__":
     parser = argparser.init_parser()
     args = parser.parse_args()
     
     pages = args.p 
     languages = map(lambda x: x.upper(), args.l)
-    status_codes = map(lambda x: x.upper(), args.sc)
+    status = map(lambda x: x.upper(), args.sc)
     problem_code = args.pc.upper()
 
     extensions = utils.load_config_file(CONFIG_FILES["EXTENSIONS"]) # .java, .c, etc ...
     language_codes = utils.load_config_file(CONFIG_FILES["LANGUAGE_CODES"]) # JAVA, C, PERL, etc ...
     status_codes = utils.load_config_file(CONFIG_FILES["STATUS_CODES"]) # AC, WA, TLE, etc ...
 
-    data = get_url_data(get_url(URL_TYPES['STATUS'], problem_code=problem_code))   
-
+    get_solutions(pages, languages, status, problem_code, language_codes, status_codes, extensions)
--- src/exception.py
+++ src/exception.py
@@ -4,4 +4,17 @@ Execption to throw if the given problem is not found
 class ProblemNotFoundException(Exception):
     def __init__(self, *arg):
         self.args = arg
-    
+
+"""
+Execption to throw if the given language is not available
+"""
+class InvalidLanguageException(Exception):
+    def __init__(self, *arg):
+        self.args = arg
+
+"""
+Execption to throw if the given status is not available
+"""
+class InvalidStatusException(Exception):
+    def __init_(self, *arg):
+        self.args = arg
