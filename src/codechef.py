58f16517f1c9b98ecd375d10279dba198969ad2c
ganeshmkumar <ganeshkumar.m1996@gmail.com>
2018-01-21 11:02:28 +0530

    Completed code to download files. Need to add some info and validations

--- src/codechef.py
+++ src/codechef.py
@@ -4,8 +4,10 @@ import requests
 
 """ Built in imports """
 from string import Template
+from os.path import expanduser
 import sys
 import re
+import os
 
 """ Custom imports """
 from exception import ProblemNotFoundException
@@ -21,12 +23,6 @@ URL_TYPES = {
     "SOLUTIONS": "solutions"
 }
 
-""" Dictionary to store url templates """
-URLS = {
-    "base": "https://www.codechef.com",
-    "status": Template("$base/status/$problem_code"),
-    "solutions": Template("$status?page=$page&language=$language&status=$status_code&handle=&Submit=GO")
-}
 
 CONFIG_FILES = {
     "EXTENSIONS": "extensions.json",
@@ -38,6 +34,12 @@ CONFIG_FILES = {
 returns a url of requested type
 """
 def get_url(url_type, **params):
+    URLS = {
+        "base": "https://www.codechef.com",
+        "status": Template("$base/status/$problem_code"),
+        "solutions": Template("$status?page=$page&language=$language&status=$status_code&handle=&Submit=GO")
+    }
+
     if url_type == URL_TYPES['BASE']:
         return URLS[url_type]
     elif url_type == URL_TYPES['STATUS']:
@@ -49,7 +51,8 @@ def get_url(url_type, **params):
 return the soup data of the given url
 """
 def get_url_data(url):
-    r = requests.get(url)
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+    r = requests.get(url, headers=headers)
     if len(r.history) == 1:
         try:
             raise ProblemNotFoundException("Error: There is no problem with the given code")
@@ -58,12 +61,12 @@ def get_url_data(url):
             sys.exit()
     return BeautifulSoup(r.text, "html.parser")
 
-def is_solutions_available(data):
+def is_solutions_not_available(data):
     return str(data).find('No Recent Activity') > -1
 
 def get_total_solution_pages(data):
     page_info = data.find("div", {"class": "pageinfo"})
-    if is_solutions_available(data):
+    if is_solutions_not_available(data):
         return 0
     elif page_info == None:
         return 1
@@ -86,25 +89,57 @@ def get_solution_details(data):
     user_names = get_user_names(solutions_table)
     solution_links = get_solution_links(solutions_table)
     return {"user_names": user_names, "solution_links": solution_links}
-    
 
+def extract_solution_code(data):
+    codes_list = data.find("div", {"id": "solutiondiv"}).find('ol').findAll('li')
+    return '\n'.join([code_line.text for code_line in codes_list]).encode('utf-8').strip()
+
+def save_solution(solution_code, solution_id, user_name, extension, problem_code, language, status):
+    HOME_DIR = expanduser("~") 
+    PROBLEM_DIR = HOME_DIR + '/Codechef_solution_downloader/' + problem_code + '/' + language + '/' + status
+    problem_file = os.path.normpath(PROBLEM_DIR + '/' + user_name + '_' + solution_id + extension)
+    if not os.path.isdir(PROBLEM_DIR):
+        os.makedirs(PROBLEM_DIR)
+
+    with open(problem_file, 'w') as file_handle:
+        print "Writing solution to the file " + problem_file
+        file_handle.write(solution_code)
+
+def download_solutions(solution_links, user_names, extension, problem_code, language, status):
+    base_url = get_url(URL_TYPES['BASE'])
+    print "Downloading " + status + " solutions for " + language
+    for i in range(0, len(solution_links)):
+        url = base_url + solution_links[i]
+        data = get_url_data(url)
+        solution_code = extract_solution_code(data)
+        save_solution(solution_code, solution_links[i].split('/')[2], user_names[i], extension, problem_code, language, status)
+        
 def get_solutions(pages, languages, status, problem_code, language_codes, status_codes, extensions):
     try:
         for lang_key in languages:
             if lang_key not in language_codes:
                 raise InvalidLanguageException("Error: There is no language named " + lang_key)
-            
             for status_key in status:
                 if status_key not in status_codes:
                     raise InvalidStatusException("Error: There is no status named " + status_key)
-                url = get_url(URL_TYPES['SOLUTIONS'], problem_code=problem_code, language=language_codes[lang_key], status_code=status_codes[status_key], page=0)
-                data = get_url_data(url)
-                pages = get_total_solution_pages(data)
-                
-                for page in range(0, pages):
-                    url = get_url(URL_TYPES['SOLUTIONS'], problem_code=problem_code, language=language_codes[lang_key], status_code=status_codes[status_key], page=page)
+                if pages == -1:
+                    url = get_url(URL_TYPES['SOLUTIONS'], problem_code=problem_code, language=language_codes[lang_key], status_code=status_codes[status_key], page=0)
                     data = get_url_data(url)
+                    total_pages = get_total_solution_pages(data)
+                else:
+                    total_pages = pages
+                
+                for page in range(0, total_pages):
+                    url = get_url(URL_TYPES['SOLUTIONS'], problem_code=problem_code, language=language_codes[lang_key], status_code=status_codes[status_key], page=page)
+                    print "Getting solutions from " + url
+                    data = get_url_data(url)
+                    if get_total_solution_pages(data) == 0:
+                        total_pages = 0
+                        break
                     solution_details = get_solution_details(data)
+                    download_solutions(solution_details["solution_links"], solution_details["user_names"], extensions[lang_key], problem_code, lang_key, status_key)
+                if total_pages == 0:
+                    print "No " + status_key + " solutions available in " + lang_key
     except (ProblemNotFoundException, InvalidLanguageException, InvalidStatusException) as e:
         print ''.join(e.args)
         sys.exit()                
